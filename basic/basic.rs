249d2048e823c5d43a2d40290b7bf6b44669a86a
ajitj <ajitk123@gmail.com>
2025-07-30 10:13:43 +0530

    basic update

---
+++ basic/basic.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello World");
+
+    let x = 5;
+    println!("The value of x is: {}", x);
+
+    //find the average of 5 digit
+    let mut y = 5;
+    y = y + 1;
+    println!("The value of y is: {}", y);
+
+}
